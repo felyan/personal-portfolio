053896c09d404803c2682b01b350afb14d7769de
Éva, Kállai Szilárdné <kallai.szilardne@gmail.com>
2023-04-13 13:13:20 +0200

    menu

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // menu icon navbar
 
-let menuIcon = documnet.querySelector("#menu-icon");
-let navbar = documnet.querySelector(".navbar");
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 // scroll sections active link
 
@@ -29,6 +34,10 @@ window.onscroll = () => {
   let header = document.querySelector(".header");
 
   header.classList.toggle("sticky", window.scrollY > 100);
+
+  // remove menu icon navbar when click navbar link (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
 // swiper
